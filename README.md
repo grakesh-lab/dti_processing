837b21850f732f3d51cace60fc89500611db18f5
Pavan Anand <pavan.anand@pm.me>
2022-11-27 00:25:19 -0500

    feat!: updated interface; split output directories
    BREAKING CHANGE: there are now 3 positional arguments for the script: 1) $MODE, 2) $INPUT, & 3) $OUTPUT.
    
    BREAKING CHANGE: $OUTPUT_PARENT deprecated, use $DERIVATIVES & $ANALYSIS instead.
    
    Overhauled script help output to reflect new interface.
    
    Outputs from the script have been separated: derivatives from the pre-processing steps (i.e., steps 1-3) are still stored under "derivatives" (location defined by $DERIVATIVES) but data derived from analysis of the derivatives (i.e., step 4) are now stored under "analysis" (location defined by $ANALYSIS).

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# Rakesh Lab DTI Processing Pipeline
-
-## Background
-
-This script was created by Pavan Anand, M.D. as a member of the University of Kentucky Lab for Addiction Neuromodulation in 2022.
+# DTI Processing Pipeline
 
 ## Purpose
 
@@ -20,34 +16,37 @@ The script expects to work on (and indeed, assumes) a BIDS-conformant file hiera
 1. Our lab's data were structured as such for use with other BIDS-reliant applications
 2. A standardized file hierarchy eases logistics of scripting (e.g., where to place the output directory or where the input files are located within the target)
 
-### `preprocess.sh`
+### Syntax
 
-#### Syntax
-
-`$ preprocess.sh [ -h | -c | -w | -s SESSION | -b SUBJECT | -p PROJECT ] OUTPUT`
+`$ preprocess.sh [ -h | -c | -w ] MODE INPUT OUTPUT`
 
 Options:
 
 * `-h`: Display help text
 * `-c`: Display copyright text
 * `-w`: Display warranty text
-* `-s SESSION`: Initiate single session-level analysis on `SESSION`, a directory containing data from an individual session
-  * "[Session](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#session)" refers to the BIDS definition of a session
-* `-b SUBJECT`: Initiate single subject-level (i.e., multi-session) analysis on `SUBJECT`, a directory containing one subdirectory for each individual session of data collected for a subject
-  * Must contain at least one session subdirectory
-  * "[Session](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#session)" & "[subject](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#subject)" refer to their respective BIDS definitions
-* `-p PROJECT`: Initiate project-level (i.e., multi-subject) on `PROJECT`, a directory containing one subdirectory for each subject participating in the study, each with respective subdirectories for all of a subject's session
-  * Must contain at least one subject with at least one session for that subject
-  * "[Session](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#session)," "[subject](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#subject)," & "[project](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#project)" refer to their respective BIDS definitions
 
 Positional parameters:
 
-* `OUTPUT`: A name for the directory in which the temporary & permanent output data will be stored. Temporary data are deleted at the script's end. The script automatically locates the project root directory for any session, subject, or project directory passed as input data to the project. It also creates a `derivatives` directory outside the project root (within the project root's parent directory) if it doesn't exist. If `OUTPUT` does not exist within `derivatives`, it creates the directory; if it does, it uses that directory.
+* `MODE`: Specify which mode the program should run in. There are 3 acceptable values to pass here: [`session`](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#session), [`subject`](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#subject), or [`project`](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#project). Each of these terms refer to their respective BIDS definitions (see hyperlinks for each term).
+  * `session`: Analyze single session
+  * `subject`: Analyze all sessions of a single subject
+  * `project`: Analyze all sessions of all subjects
+* `INPUT`: Directory containing input data for chosen `MODE`
+* `OUTPUT`: A name for the directory in which the temporary & permanent output data will be stored. Temporary data are deleted at the script's end. The script automatically locates the "BIDS root directory" (i.e., the parent folder of the [project](https://bids-standard.github.io/bids-starter-kit/folders_and_files/folders.html#subject)) for any session, subject, or project directory passed as input data to the project. It also creates a `derivatives` & `analysis` directories outside the project root (within the project root's parent directory) if it doesn't exist. If `OUTPUT` does not exist within `derivatives`, it creates the directory; if it does, it uses that directory.
 
-**CAUTION**: in cases in which `derivatives` exists and there is already a subdirectory of the same name passed to `OUTPUT`, the program *may* overwrite data within that subdirectory should there be a conflict of subdirectory names within `derivatives/OUTPUT`
+**CAUTION**: in cases in which `derivatives` and/or `analysis` exist(s) and there is already a subdirectory within either of the same name passed to `OUTPUT`, the program *may* overwrite data within that subdirectory should there be a conflict of subdirectory names within `[derivatives | analysis]/OUTPUT`
+
+## Acknowledgements
+
+This script was created by Pavan Anand, M.D. as a member of the University of Kentucky Lab for Addiction Neuromodulation in 2022.
 
 ## CHANGELOG
 
-### version 0.2.0
+### version 0.3.0
 
-* feat: integrated TBSS pipeline MVP
+* feat!: updated interface; split output directories
+  * BREAKING CHANGE: there are now 3 positional arguments for the script: 1) $MODE, 2) $INPUT, & 3) $OUTPUT.
+  * BREAKING CHANGE: $OUTPUT_PARENT deprecated, use $DERIVATIVES & $ANALYSIS instead.
+  * Overhauled script help output to reflect new interface.
+  * Outputs from the script have been separated: derivatives from the pre-processing steps (i.e., steps 1-3) are still stored under "derivatives" (location defined by $DERIVATIVES) but data derived from analysis of the derivatives (i.e., step 4) are now stored under "analysis" (location defined by $ANALYSIS).
--- preprocess.sh
+++ preprocess.sh
@@ -4,8 +4,9 @@
 #
 # Depends upon package "coreutils" for command "realpath":
 #   $ brew install coreutils
+# TODO: add check for coreutils program & error if unfound
 
-readonly VERSION="0.2.0"
+readonly VERSION="0.3.0"
 readonly PROGRAM="$(basename $0)"
 readonly AUTHORS="Pavan A. Anand"
 readonly COPYRIGHT_YEARS="2022"
@@ -13,6 +14,11 @@ readonly COPYRIGHT_YEARS="2022"
 # Adding boolean logic aliases
 readonly TRUE=1
 readonly FALSE=0
+
+# Aliases for positional arguments
+readonly MODE=$1
+readonly INPUT=$2
+readonly OUTPUT=$3
 
 print_purpose() {
   echo "
@@ -29,19 +35,40 @@ IMPORTANT: Ensure that your data are structured in a BIDS-conformant manner.
 print_help() {
   echo "
 Usage:
-  $ ${PROGRAM} [ -h | -c | -w | -s SESSION | -b SUBJECT | -p PROJECT ] OUTPUT
+  $ ${PROGRAM} [ -h | -c | -w ] MODE INPUT OUTPUT
 
 Options:
   h  Print this help message and exit
   c  Print copyright & acknowledgement information
   w  Print warranty information
-  s  Analyze files within a single session
-  b  Analyze all runs for a single subject
-  p  Analyze all runs of several subjects
 
 Positional arguments:
-  OUTPUT  Directory under which \"derivatives\" directory will be created
-          to store outputs from preprocessing steps"
+  MODE    Specify which mode the program should run in
+  INPUT   Directory containing input data for chosen MODE
+  OUTPUT  Name of directories within \"<BIDS_ROOT>/derivatives\" and
+          \"<BIDS_ROOT>/analysis\" used to store script outputs
+
+Acceptable values for MODE:
+  "session": Analyze single session
+  "subject": Analyze all sessions of a single subject
+  "project": Analyze all sessions of all subjects
+
+Example usage:
+  $ ${PROGRAM} subject ~/data/flanker/sub-01 flanker_analysis
+
+This will run an analysis on all sessions for subject 1 within the \"flanker\"
+project. BIDS_ROOT, in this case, is \"~/data\", as it houses the project
+\"~/data/flanker\" on which the subject-level analysis is to be performed. It
+will search for (and create, if absent) a \"derivatives\" and \"analysis\" sub-
+directories under BIDS_ROOT. Inside each, it will create another subdirectory
+called \"flanker_analysis\". Altogether, the outputs will be stored under:
+  \"~/data/derivatives/flanker_analysis\" & \"~/data/analysis/flanker_analysis\""
+  
+# TODO: add examples & more in-depth documentation to README
+# TODO: uncomment following line when examples/extra info added to README
+# See README for more examples and in-depth documentation."
+
+exit 1
 }
 
 print_copyright() {
@@ -72,15 +99,13 @@ exit_error() {
   #######################################
   # Exit program after printing custom error message
   # Arguments:
-  #  $1: Custom error message, a string
+  #   $1: Custom error message, a string
   # Outputs:
   #  Writes error message to STDOUT
   #######################################
   echo "Error: $1"  # TODO: consider printing to STDERR (+/- STDOUT)
   exit 1  # TODO: allow use of different exit codes
 }
-
-# TODO: add verbosity toggle function
 
 create_dir(){
   #######################################
@@ -134,7 +159,7 @@ eddy_correction(){
   # Perform eddy current correction
   # Globals:
   #   selected_file: used
-  #   working_dir:   used
+  #   tmp_dir:   used
   #   eddy_output:   modified
   # Arguments:
   #   $1: Input, an .nii file
@@ -144,7 +169,9 @@ eddy_correction(){
   #######################################
   echo "Correcting for eddy currents within ${selected_file}..."
   # WARN: does *not* like spaces anywhere in file path arguments!
-  eddy_output="${working_dir}/${2}_eddy_corrected"
+  # TODO: redefine tmp_dir as global, rename to all-caps to signify
+  # WARN: Using local, readonly variable ($tmp_dir) in separate function...
+  eddy_output="${tmp_dir}/${2}_eddy_corrected"
   eddy_correct $1 ${eddy_output} 0
 }
 
@@ -152,7 +179,7 @@ brain_extraction(){
   #######################################
   # Skull strip the input image
   # Globals:
-  #   working_dir:   used
+  #   tmp_dir:   used
   #   file_basename: used
   #   bet_output:    modified
   # Arguments:
@@ -163,7 +190,9 @@ brain_extraction(){
   #######################################
   echo "Extracting binary brain mask..."
   # TODO: investigate feasibility of using `bet2` instead
-  bet_output="${working_dir}/${2}_${file_basename}"
+  # TODO: redefine tmp_dir as global, rename to all-caps to signify
+  # WARN: Using local, readonly variable ($tmp_dir) in separate function...
+  bet_output="${tmp_dir}/${2}_${file_basename}"
   bet "$1" ${bet_output} -F -f .3
 }
 
@@ -202,34 +231,34 @@ point the script to an existing BIDS-compliant directory."
   local -r subject_id="$(basename $(dirname $(pwd)))"
   local -r session_id="$(basename $(pwd))"
   local -r prefix="${subject_id}_${session_id}"
-  local -r session_outputs="${OUTPUT_PARENT}/${prefix}"
-  local -r working_dir="${session_outputs}/tmp"
+  local -r session_outputs="${DERIVATIVES}/${prefix}"
+  local -r tmp_dir="${session_outputs}/tmp"
 
   create_dir ${session_outputs} ${TRUE}
-  create_dir ${working_dir} ${FALSE}
+  create_dir ${tmp_dir} ${FALSE}
 
   local -r file_basename="brain"
 
   identify_file "${session}/dwi" "*.nii.gz"
-  echo "Unzipping ${selected_file} to ${working_dir}..."
+  echo "Unzipping ${selected_file} to ${tmp_dir}..."
   local filename="${prefix}_${file_basename}.nii"
-  gunzip < "${selected_file}" > "${working_dir}/${filename}"
+  gunzip < "${selected_file}" > "${tmp_dir}/${filename}"
 
   # Correct eddy currents
-  eddy_correction "${working_dir}/${filename}" "${prefix}"
+  eddy_correction "${tmp_dir}/${filename}" "${prefix}"
 
   # Skull strip eddy-corrected images
   brain_extraction "${eddy_output}" "${prefix}"
 
-  # Cleanup
+  # Cleanup unneeded intermediary files
   rm "${bet_output}.nii.gz"
   mv "${bet_output}_mask.nii.gz" \
      "${bet_output}_mask-nodif.nii.gz"
 
   # Select first image in temporal sequence of eddy-corrected scan
   echo "Splitting eddy-corrected scan into snapshots..."
-  fslsplit "${eddy_output}" "${working_dir}/split" -t
-  mv "${working_dir}/split0000.nii.gz" "${bet_output}-nodif.nii.gz"
+  fslsplit "${eddy_output}" "${tmp_dir}/split" -t
+  mv "${tmp_dir}/split0000.nii.gz" "${bet_output}-nodif.nii.gz"
 
   # Run dtifit
   echo "Fitting diffusion tensor model at each voxel..."
@@ -242,8 +271,8 @@ point the script to an existing BIDS-compliant directory."
     -r "${bvec}" \
     -b "${bval}"
 
-  # Remove "${session_outputs}/tmp" directory
-  rm -rf "${working_dir}"
+  # Cleanup remaining temporary/intermediary files
+  rm -rf "${tmp_dir}"
 }
 
 analyze_subject() {
@@ -254,9 +283,7 @@ analyze_subject() {
   #######################################
   subject=$1
   local session
-  for session in $(ls -d ${subject}/*); do
-    analyze_session ${session}
-  done
+  for session in $(ls -d ${subject}/*); do analyze_session ${session}; done
 }
 
 analyze_project() {
@@ -267,76 +294,66 @@ analyze_project() {
   #######################################
   project=$1
   local subject
-  for subject in $(ls -d ${project}/*); do
-    analyze_subject ${subject}
-  done
+  for subject in $(ls -d ${project}/*); do analyze_subject ${subject}; done
 }
 
-# TODO: add "m" flag to encapsulate mode behavior & simplify interface
-opts="hcws:b:p:"
+opts="hcw"  # TODO: add -v/--verbose option
 while getopts ${opts} option; do
   case ${option} in
-    h) print_purpose && print_help && exit ;;
+    h) print_purpose && print_help ;;
     c) print_copyright ;;
     w) print_warranty ;;
-    s) 
-      mode="session"
-      data_dir="${OPTARG}"
-      bids_parent="$(realpath ${data_dir}/../../../)"
-      ;;
-    b)
-      mode="subject"
-      data_dir="${OPTARG}"
-      bids_parent=$(realpath ${data_dir}/../..)
-      ;;
-    p)
-      mode="project"
-      data_dir="${OPTARG}"
-      bids_parent=$(realpath ${data_dir}/..)
-      ;;
-    \?) print_help && exit ;;
+    \?) print_help ;;
   esac
 done
 
-shift $((${OPTIND} - 1))
+shift $((${OPTIND} - 1))  # TODO: test what happens if no opts given
 
-if [ -z "$1" ] && [ -z "${OUTPUT_PARENT}" ]; then
-  exit_error "\"OUTPUT\" not specified"
-elif [ -z "$1" ] && [ ! -z "${OUTPUT_PARENT}" ]; then
-  echo "DEBUG: \"OUTPUT_PARENT\" already specified (\"${OUTPUT_PARENT}\")"
+# TODO: check that all cases are covered
+if [ -z "$3" ] && ( [ -z "${DERIVATIVES}" ] || [ -z "${ANALYSIS}" ] ); then
+  exit_error "\"OUTPUT\" not specified"  # Define this behavior for MODE/INPUT
+elif [ -z "$3" ] && [ ! -z "${DERIVATIVES}" ] && [ ! -z "${ANALYSIS}" ]; then
+  echo "DEBUG: using \"DERIVATIVES\" & \"ANALYSIS\" as defined in environment"
 else
-  echo "DEBUG: \"OUTPUT_PARENT\" specified as \"$1\""
-  OUTPUT_PARENT="${bids_parent}/derivatives/$1"
-  export OUTPUT_PARENT="${bids_parent}/derivatives/$1"
+  echo "DEBUG: \"OUTPUT\" specified as \"${OUTPUT}\""  
 fi
 
-create_dir ${OUTPUT_PARENT} ${FALSE}
-
-case ${mode} in
+readonly notice="DEBUG: Starting ${1}-level analysis."
+case ${MODE} in
   "session")
-    echo "DEBUG: Starting session-level analysis."
-    analyze_session ${data_dir}
+    echo ${notice}
+    bids_root=$(realpath ${INPUT}/../../..)
+    DERIVATIVES="${bids_root}/derivatives/${OUTPUT}"
+    create_dir ${DERIVATIVES} ${FALSE}
+    analyze_session ${INPUT}
     ;;
   "subject")
-    echo "DEBUG: Starting subject-level analysis."
-    analyze_subject ${data_dir}
+    echo ${notice}
+    bids_root=$(realpath ${INPUT}/../..)
+    DERIVATIVES="${bids_root}/derivatives/${OUTPUT}"
+    create_dir ${DERIVATIVES} ${FALSE}
+    analyze_subject ${INPUT}
     ;;
   "project")
-    echo "DEBUG: Starting project-level analysis."
-    analyze_project ${data_dir}
+    echo ${notice}
+    bids_root=$(realpath ${INPUT}/..)
+    DERIVATIVES="${bids_root}/derivatives/${OUTPUT}"
+    create_dir ${DERIVATIVES} ${FALSE}
+    analyze_project ${INPUT}
     ;;
+  # TODO: handle unknown alternative cases
 esac
 
-# TODO: rewrite following code to be modular & parallel-izable
-# TODO: split code off into separate script if needed to do so
-readonly TBSS_DIR="${OUTPUT_PARENT}/analysis"
-create_dir "${TBSS_DIR}" ${FALSE}
+# TODO: add option to disable TBSS
+# TODO: add option to enable saving of all intermediate/temporary files
+readonly ANALYSIS="${bids_root}/analysis/${OUTPUT}"
+create_dir "${ANALYSIS}" ${FALSE}
 # TODO: make use of select_file for the following
 # TODO: modify select_file to be compatible with above requirement
-for file in $(find "${OUTPUT_PARENT}" -name "*_FA.nii.gz"); do
-  cp "${file}" "${TBSS_DIR}"
+for file in $(find "${DERIVATIVES}" -name "*_FA.nii.gz"); do
+  cp "${file}" "${ANALYSIS}"
 done
-cd ${TBSS_DIR}
+cd ${ANALYSIS}
 tbss_1_preproc *.nii.gz
 tbss_2_reg -T
 tbss_3_postreg -S
